bce9d5884c9ddcc490f439b737a92447066fdf2c
Sajal <nospamsajal@gmail.com>
2023-07-24 21:41:42 +0545

    Upvote Post Handler Implemented

--- Application/Posts/Command/UpvotePost/UpvotePostCommandHandler.cs
+++ Application/Posts/Command/UpvotePost/UpvotePostCommandHandler.cs
@@ -2,11 +2,6 @@
 using Application.Common.Interface.Persistence;
 using Domain.CompanySpace.ValueObjects;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Posts.Command.UpvotePost;
 
